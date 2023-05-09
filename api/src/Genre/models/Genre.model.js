58e4535acd12bdaa8d96d765481cb4add857092e
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-09 12:29:43 -0600

    feat(Genre/models): :sparkles: Creacion del modelo Genre
    Definimos los datos que definen a nuestro modelo Genre, y establecimos los contrainst y las validaciones que va a tener cada campo de nuestro modelo

---
+++ api/src/Genre/models/Genre.model.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (database) => {
+  database.define('genre', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true,
+      validate: {
+        isUUID: true
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 50]
+      }
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isLowercase: true,
+        len: [2, 50]
+      }
+    },
+    games_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+        isInt: true
+      }
+    },
+    image_background: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    }
+  }, {
+    timestamps: false
+  })
+}
