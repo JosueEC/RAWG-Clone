440303e3922f540a6a9af8b6b65bc9e4fbafcc82
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-09 09:35:27 -0600

    feat(src): :sparkles: Creacion del la configuracion del servidor
    Definimos el bloque de codigo para los cors, hicimos uso del middleware morgan para desarrollo, creamos una ruta de ejemplo para pruebas y definimos una ruta que atrapa los errores y los muestra en consola ademas de devolverlo como respuesta

---
+++ api/src/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const morgan = require('morgan')
+
+const server = express()
+
+server.use((req, res, next) => {
+  res.header('Acces-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Acces-Control-Allow-Credentials', 'true')
+  res.header('Acces-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Acces-Controll-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
+
+server.use(morgan('dev'))
+
+server.get((req, res) => {
+  res.status(200).send('Hello World')
+})
+
+server.use((err, req, res, next) => {
+  const status = err.status || 500
+  const message = err.message || err
+  console.log(err)
+  res.status(status).send(message)
+})
+
+module.exports = server
