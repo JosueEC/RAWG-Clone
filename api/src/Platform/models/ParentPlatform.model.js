f6dd0f556b78908487e50ccaed23bce25f839c82
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-11 13:54:02 -0600

    feat(Videogame/models): :sparkles: Creacion del modelo Videogame
    Este modelo tiene las propiedades de id, name, released, background_image, website, rating, rating_top, ratings, movies_count, ratings_count y description_raw. Por ahora estas son las propiedades que hemos definido, pero pueden cambiar mas adelante

---
+++ api/src/Platform/models/ParentPlatform.model.js
@@ -0,0 +1,11 @@
+// const { DataTypes } = require('sequelize')
+
+// module.exports = (database) => {
+//   database.define('parent_platform', {
+//     id: {
+//       type: DataTypes.
+//     },
+//     name: {},
+//     slug: {}
+//   })
+// }
---
+++ api/src/Videogame/models/Videogame.model.js
@@ -0,0 +1,98 @@
+const { DataTypes } = require('sequelize')
+
+module.exports = (database) => {
+  database.define('videogame', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true,
+      validate: {
+        isUUid: true
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 50]
+      }
+    },
+    released: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        is: /^\\d{4}-\\d{2}-\\d{2}$/i
+      }
+    },
+    background_image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    },
+    website: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+        isInt: true
+      }
+    },
+    rating_top: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+        isInt: true
+      }
+    },
+    ratings: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+        isInt: true
+      }
+    },
+    movies_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+        isInt: true
+      }
+    },
+    ratings_count: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate: {
+        isNumeric: true,
+        isInt: true
+      }
+    },
+    description_raw: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: 'No Description',
+      validate: {
+        len: [10, 1600]
+      }
+    }
+  }, {
+    timestamps: false
+  })
+}
