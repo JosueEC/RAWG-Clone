38d24690dc785756e99cf490393a48eeffe7a940
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-11 17:45:09 -0600

    feat(Videogame/controllers): :sparkles: Creacion del archivo controller para el modelo Videogame
    En este archivo crearemos la logica que maneja las peticiones HTTP, por ahora solo se ha creado el archivo, aun falta desarrollar la logica para las diferentes peticiones HTTP

---
+++ api/src/Videogame/controllers/Videogame.controller.js
@@ -0,0 +1,7 @@
+const getVideogames = async (req, res) => {
+  res.status(200).send('get videogames en el controller')
+}
+
+module.exports = {
+  getVideogames
+}
--- api/src/Videogame/routes/Videogame.routes.js
+++ api/src/Videogame/routes/Videogame.routes.js
@@ -1,13 +1,13 @@
 const { Router } = require('express')
 
+const VideogameController = require('../controllers/Videogame.controller')
+
 const router = Router()
 
 // TODO: Por defecto obtiene 100 videojuegos de la API externa y 50 de la base de datos
 
 // TODO: En caso de que llegue una query en la request, obtiene 100 videojuegos de la API externa y 50 videojuegos de la base de datos, loc cuales, coincidan con la query por nombre
-router.get('/', (req, res) => {
-  res.status(200).send('get -> videogames')
-})
+router.get('/', VideogameController.getVideogames)
 
 // TODO: Obtiene la informacion detallada de un videojuego en base a un ID recibido por params
 router.get('/:idVideogame', (req, res) => {
