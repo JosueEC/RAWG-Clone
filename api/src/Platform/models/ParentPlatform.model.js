ea2ed36b25f154762487ac217b17b9492e290e56
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-11 14:02:24 -0600

    feat(Platform/models): :sparkles: Creacion del modelo ParentPlatform
    Este modelo tiene las propiedades id, name y slug, las cuales van a definir las plataformas padre de las cuales se desglozan las plataformas hija

--- api/src/Platform/models/ParentPlatform.model.js
+++ api/src/Platform/models/ParentPlatform.model.js
@@ -1,11 +1,32 @@
-// const { DataTypes } = require('sequelize')
+const { DataTypes } = require('sequelize')
 
-// module.exports = (database) => {
-//   database.define('parent_platform', {
-//     id: {
-//       type: DataTypes.
-//     },
-//     name: {},
-//     slug: {}
-//   })
-// }
+module.exports = (database) => {
+  database.define('parent_platform', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV1,
+      primaryKey: true,
+      validate: {
+        isUUid: true
+      }
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 50]
+      }
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 50]
+      }
+    }
+  }, {
+    timestamps: false
+  })
+}
