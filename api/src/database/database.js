b970465883dde924da799cf56375d38c35281d5a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-11 16:31:17 -0600

    feat(database): :sparkles: Definicion de los modelos videogame, genre, parentplatform y childplatform
    Importamos las funciones que definen a estos modelos y las ejecutamos pasandole la conexion con sequelize, de igual forma establecimos la relacion many to many entre videogame y genre

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -3,11 +3,27 @@ const { Sequelize } = require('sequelize')
 
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env
 
+const defineModelVideogame = require('../Videogame/models/Videogame.model')
+const defineModelGenre = require('../Genre/models/Genre.model')
+const defineModelParentPlatform = require('../Platform/models/ParentPlatform.model')
+const defineModelChildPlatform = require('../Platform/models/ChildPlatform.model')
+
 const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false,
   native: false
 })
 
+defineModelVideogame(database)
+defineModelGenre(database)
+defineModelParentPlatform(database)
+defineModelChildPlatform(database)
+
+const { videogame, genre } = database.models
+
+videogame.belongsToMany(genre, { through: 'videogame-genre' })
+genre.belongsToMany(videogame, { through: 'videogame-genre' })
+
 module.exports = {
-  database
+  database,
+  ...database.models
 }
