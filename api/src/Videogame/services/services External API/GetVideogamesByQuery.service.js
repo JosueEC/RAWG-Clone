4d75d52de626f2de98948a1447e0b2ee1782fe87
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-12 16:35:37 -0600

    feat(Videogame/services/API): :sparkles: Definicion de la logica para formatera los results por query
    Usamos los mismos formatters que en el endpointque obtiene las primeras recetas por defecto, estas funciones trabajan igual para los results obtenidos al hacer una peticion por query

--- api/src/Videogame/services/services External API/GetVideogamesByQuery.service.js
+++ api/src/Videogame/services/services External API/GetVideogamesByQuery.service.js
@@ -3,15 +3,18 @@ const { API_KEY } = process.env
 const { EXTERNAL_API_CONST } = require('../../../../consts')
 const fetch = require('node-fetch')
 
+const { formatArrayOfVideogames } = require('./formatters/formatArrayOfVideogames')
+
 const serviceGetVideogamesByQueryFromAPI = async (name) => {
   // ! No. Por nombre: "https://api.rawg.io/api/games?key=123&search=gta&page_size=1"
-  const videogames = fetch(`${EXTERNAL_API_CONST.DOMAIN}/games?key=${API_KEY}&search=${name}&${EXTERNAL_API_CONST.PAGE_SIZE}`)
+  const videogames = await fetch(`${EXTERNAL_API_CONST.DOMAIN}/games?key=${API_KEY}&search=${name}&${EXTERNAL_API_CONST.PAGE_SIZE}`)
     .then((response) => response.json())
     .then((data) => {
       return data
     })
 
-  return videogames
+  const newFormatVideogames = formatArrayOfVideogames(videogames)
+  return newFormatVideogames
 }
 
 module.exports = {
