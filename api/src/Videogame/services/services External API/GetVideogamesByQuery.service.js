70bbf11b212d3ebffe4d1569b565e505b34796e9
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-12 16:19:52 -0600

    feat(Videogame/services/API): :sparkles: Definicion del serviceGetVideogamesByQueryFromAPI
    Nuestro service ya devuelve los resultados que coinciden con la query que llega en la request, por el momento devuelve toda la informacion, aun falta formatera esa informacion para devolver solo los datos que van a ser requeridos en el frontend

--- api/src/Videogame/services/services External API/GetVideogamesByQuery.service.js
+++ api/src/Videogame/services/services External API/GetVideogamesByQuery.service.js
@@ -1,5 +1,17 @@
-const serviceGetVideogamesByQueryFromAPI = async (name) => {
+require('dotenv').config()
+const { API_KEY } = process.env
+const { EXTERNAL_API_CONST } = require('../../../../consts')
+const fetch = require('node-fetch')
 
+const serviceGetVideogamesByQueryFromAPI = async (name) => {
+  // ! No. Por nombre: "https://api.rawg.io/api/games?key=123&search=gta&page_size=1"
+  const videogames = fetch(`${EXTERNAL_API_CONST.DOMAIN}/games?key=${API_KEY}&search=${name}&${EXTERNAL_API_CONST.PAGE_SIZE}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data
+    })
+
+  return videogames
 }
 
 module.exports = {
