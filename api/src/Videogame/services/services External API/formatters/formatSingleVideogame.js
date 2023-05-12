b49eacef561077d3a97bcc150f8f4af72a81d17a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-12 15:38:23 -0600

    feat(Videogame/services/API): :sparkles: Definicion de la funcion formatArrayOfPlatforms
    Esta funciona formatea las plataformas anidadas en la propiedad platforrms de nuestro videogame, y devuelve solo los datos que necesitamos

--- api/src/Videogame/services/services External API/formatters/formatSingleVideogame.js
+++ api/src/Videogame/services/services External API/formatters/formatSingleVideogame.js
@@ -7,15 +7,40 @@ function formatSingleVideogame (objVideogame) {
     website: objVideogame.website,
     rating: objVideogame.rating,
     rating_top: objVideogame.rating_top,
-    ratings: objVideogame.ratings,
+    ratings: formatArrayOfRatings(objVideogame.ratings),
     movies_count: objVideogame.movies_count,
     ratings_count: objVideogame.movies_count,
-    platforms: objVideogame.platforms,
+    platforms: formatArrayOfPlatforms(objVideogame.platforms),
     genres: objVideogame.genres,
     tags: objVideogame.tags,
     description_raw: objVideogame.description_raw
   }
   return videogame
+}
+
+function formatArrayOfRatings (arrayOfRatings) {
+  const ratings = arrayOfRatings.map((rating) => {
+    return {
+      id: rating.id,
+      title: rating.title,
+      count: rating.count
+    }
+  })
+  return ratings
+}
+
+function formatArrayOfPlatforms (arrayOfPlatforms) {
+  const platforms = arrayOfPlatforms.map((item) => {
+    return {
+      id: item.platform.id,
+      name: item.platform.name,
+      image_background: item.platform.image_background,
+      released_at: item.released_at,
+      requirements: item.requirements
+    }
+  })
+
+  return platforms
 }
 
 module.exports = {
