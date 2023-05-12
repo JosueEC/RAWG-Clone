e2394ae321531bf48a6b464b725d60d1d971dcb2
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-12 09:56:05 -0600

    feat(Videogame/services/API): :sparkles: Se realizo un filtro general de los resultados de la API externa
    Obutvimos los datos que necesitamos de forma general, aun falta filtrar datos que se encuntran de forma anidada en el el arhivo JSON

--- api/src/Videogame/services/services External API/formatters/formatArrayOfVideogames.js
+++ api/src/Videogame/services/services External API/formatters/formatArrayOfVideogames.js
@@ -1,5 +1,29 @@
 function formatArrayOfVideogames (arrayOfVideogames) {
+  const results = arrayOfVideogames.results.map((videogame) => {
+    return {
+      id: videogame.id,
+      slug: videogame.slug,
+      name: videogame.name,
+      released: videogame.released,
+      background_image: videogame.background_image,
+      rating: videogame.rating,
+      rating_top: videogame.rating_top,
+      ratings: videogame.ratings,
+      ratings_count: videogame.ratings_count,
+      platforms: videogame.platforms,
+      genres: videogame.genres,
+      tags: videogame.tags,
+      short_screenshots: videogame.short_screenshots
+    }
+  })
 
+  const newFormat = {
+    next: arrayOfVideogames.next,
+    previous: arrayOfVideogames.previous,
+    results
+  }
+
+  return newFormat
 }
 
 module.exports = {
