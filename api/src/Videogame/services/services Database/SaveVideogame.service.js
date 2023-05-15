41ad47ca868a3a03b3a10628301eca299d1815f7
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 16:45:34 -0600

    feat(Videogame/services/Database): :sparkles: Definicion del serviceSaveVideogameInDatabase
    Creamos la logica que almacena el nuevo videogame en la base de datos
    
    * Hasta aqui es necesario realizar cambios en el modelo videogame para no tener problemas con la creacion de nuevos videogame en la base de datos

--- api/src/Videogame/services/services Database/SaveVideogame.service.js
+++ api/src/Videogame/services/services Database/SaveVideogame.service.js
@@ -1,6 +1,27 @@
+//! Models
+const { videogame } = require('../../../database/database')
+
+//! Service
 /* eslint-disable camelcase */
 const serviceSaveVideogameInDatabase = async ({ id, name, released, background_image, website, rating, rating_top, ratings, movies_count, ratings_count, platforms, genres, tags, description_raw }) => {
-  return 'service saveVideogame in Database'
+  const newVideogame = await videogame.create({
+    id,
+    name,
+    released,
+    background_image,
+    website,
+    rating,
+    rating_top,
+    ratings,
+    movies_count,
+    ratings_count,
+    genres,
+    tags,
+    description_raw
+  })
+
+  newVideogame.addChildPlatforms(platforms)
+  return newVideogame
 }
 
 module.exports = {
