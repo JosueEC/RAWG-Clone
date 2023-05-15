8f23fd6c7b7f39c8537e20494a2785764106fada
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 16:36:09 -0600

    feat(Videogame/services/Database): :sparkles: Creacion del serviceSaveVideogameInDatabase
    A traves de este service vamos a poder almacenar nuevos videogames en la base de datos
    
    * Hasta aqui solo se creo el metodo del service, aun falta definir la logica de la funcion para que ejecute el proceso correspondiente

--- api/src/Videogame/controllers/Videogame.controller.js
+++ api/src/Videogame/controllers/Videogame.controller.js
@@ -3,6 +3,7 @@
 const { serviceGetVideogamesFromAPI } = require('../services/services External API/GetVideogames.service')
 const { serviceGetVideogameByIDFromAPI } = require('../services/services External API/GetVideogameByID.service')
 const { serviceGetVideogamesByQueryFromAPI } = require('../services/services External API/GetVideogamesByQuery.service')
+const { serviceSaveVideogameInDatabase } = require('../services/services Database/SaveVideogame.service')
 
 //! Handlers
 const getVideogames = async (req, res) => {
@@ -34,9 +35,10 @@ const postVideogame = async (req, res) => {
   try {
     const { id, name, released, background_image, website, rating, rating_top, ratings, movies_count, ratings_count, platforms, genres, tags, description_raw } = req.body
 
-    const infoQuery = `${id}, ${name}, ${released}, ${background_image}, ${website}, ${rating}, ${rating_top}, ${ratings}, ${movies_count}, ${ratings_count}, ${platforms}, ${genres}, ${tags}, ${description_raw}`
+    // const infoQuery = `${id}, ${name}, ${released}, ${background_image}, ${website}, ${rating}, ${rating_top}, ${ratings}, ${movies_count}, ${ratings_count}, ${platforms}, ${genres}, ${tags}, ${description_raw}`
+    const newVideogame = await serviceSaveVideogameInDatabase({ id, name, released, background_image, website, rating, rating_top, ratings, movies_count, ratings_count, platforms, genres, tags, description_raw })
 
-    res.status(201).send({ status: 'CREATED', data: infoQuery })
+    res.status(201).send({ status: 'CREATED', data: newVideogame })
   } catch (error) {
     res.status(404).send({ status: 'FAILED', error: error.message })
   }
---
+++ api/src/Videogame/services/services Database/SaveVideogame.service.js
@@ -0,0 +1,8 @@
+/* eslint-disable camelcase */
+const serviceSaveVideogameInDatabase = async ({ id, name, released, background_image, website, rating, rating_top, ratings, movies_count, ratings_count, platforms, genres, tags, description_raw }) => {
+  return 'service saveVideogame in Database'
+}
+
+module.exports = {
+  serviceSaveVideogameInDatabase
+}
