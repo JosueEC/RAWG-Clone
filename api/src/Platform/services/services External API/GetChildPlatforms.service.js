138441f12f1a281fb5a3046bc7743e0dcb43403c
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 13:22:14 -0600

    feat(Platform/services): :sparkles: Defincion del serviceGetChildPlatformsFromAPI
    Creamos la logica que obtiene la informacion de las child-platform desde la API externa de RAWG
    
    * Hasta aqui solo obtiene la informacion, aun es necsario crear un formatter para mapear la informacion y obtener solo los datos que se van a requerir en el frontend

--- api/src/Platform/services/services External API/GetChildPlatforms.service.js
+++ api/src/Platform/services/services External API/GetChildPlatforms.service.js
@@ -1,5 +1,18 @@
+//! Consts and Envoiroment variables
+require('dotenv').config()
+const { API_KEY } = process.env
+const { EXTERNAL_API_CONST } = require('../../../../consts')
+const fetch = require('node-fetch')
+
+//! Service
 const serviceGetChildPlatfromsFromAPI = async () => {
-  return 'service get child platforms from API'
+  //! https://api.rawg.io/api/platforms?key=123
+  const childPlatforms = fetch(`${EXTERNAL_API_CONST.DOMAIN}/platforms?key=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      return data
+    })
+  return childPlatforms
 }
 
 module.exports = {
