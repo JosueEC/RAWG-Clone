3cd0e714b423c1973e6af571538df1ccaec48bc2
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 09:41:27 -0600

    refactor(Genre/controllers): :recycle: Creamos el modulo GenreController para modularizar la logica de los manejadores de las peticiones, por ahora solo esta el concepto, aun falta desarrollar la logica de cada funcion

---
+++ api/src/Genre/controllers/Genre.controller.js
@@ -0,0 +1,7 @@
+const getGenres = async (req, res) => {
+  res.status(200).send('Controller creado y accedido')
+}
+
+module.exports = {
+  getGenres
+}
--- api/src/Genre/routes/Genre.routes.js
+++ api/src/Genre/routes/Genre.routes.js
@@ -1,10 +1,9 @@
 const { Router } = require('express')
+const GenreController = require('../controllers/Genre.controller')
 
 const router = Router()
 
-// TODO: Obtiene todos los genres a traves de dos endpoint. El primero obtiene los genres en general y el segundo obtiene informacion detallada de cada genre
-router.get('/', (req, res) => {
-  res.status(200).send('get -> genres')
-})
+// TODO: Obtiene todos los genres desde la API externa de RAWG
+router.get('/', GenreController.getGenres)
 
 module.exports = router
