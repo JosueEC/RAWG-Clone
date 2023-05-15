5072b136f07398e085a1e3744d0141410799daa6
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 13:26:32 -0600

    feat(Platform/services): :sparkles: Creacion del formatter de arrayOfChildPlatforms
    Este formatter va a mapear la informacion de las child-platforms que fue devuelta por la API externa de RAWG y devolvera solo las propiedades que se van a requerir en el frontend
    
    * Hasta aqui solo se creo el metodo, aun falta definir su logica para que realice la funcion correspondiente

---
+++ api/src/Platform/services/services External API/formatters/formatArrayOfChildPlatforms.js
@@ -0,0 +1,8 @@
+function formatArrayOfChildPlatforms (arrayOfChildPlatforms) {
+  // TODO: Definir la logica para mapear las child-platforms y obtener solo las propiedades que se requiern en el frontend
+  return 'child-platforms formateadas'
+}
+
+module.exports = {
+  formatArrayOfChildPlatforms
+}
