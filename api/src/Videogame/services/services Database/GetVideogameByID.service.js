47dabf8849226fba6a4331786f40ed45a4856f35
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-16 11:14:58 -0600

    feat(Videogame/services/Database): :sparkles: Inclusion del modelo genre en el serviceGetVideogameByIDFromDatabase
    Incluimos la propiedad slug del modelo de genre en el servicio que nos devuelve la informacion detallada de un videogame de la base de datos en base al id
    
    * Hasta aqui falta agregar el modelo tag a la respuesta del service

--- api/src/Videogame/services/services Database/GetVideogameByID.service.js
+++ api/src/Videogame/services/services Database/GetVideogameByID.service.js
@@ -1,16 +1,21 @@
 //! Models
-const { videogame, childPlatform } = require('../../../database/database')
+const { videogame, childPlatform, genre } = require('../../../database/database')
 
 //! Service
 const serviceGetVideogameByIDFromDatabase = async (idVideogame) => {
   const game = await videogame.findByPk(idVideogame, {
-    include: {
-      model: childPlatform,
-      attributes: ['id', 'name', 'image_background'],
-      through: {
-        attributes: []
+    include: [
+      {
+        model: childPlatform,
+        attributes: ['id', 'name', 'image_background'],
+        through: { attributes: [] }
+      },
+      {
+        model: genre,
+        attributes: ['slug'],
+        through: { attributes: [] }
       }
-    }
+    ]
   })
   return game
 }
