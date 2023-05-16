9fe213dea2e75c498f641db1fa3fd6603659062a
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 19:27:00 -0600

    feat(database): :sparkles: Creacion del modelo tag y su relacion con videogame
    Ejecutamos la funcion define para crear nuestro modelo tag y de igual forma importamos el modelo para establecer una relacion many-to-many con videogame, un videogame puede estar asociado a varias tag y una tag puede estar asociada a varios videogame

--- api/src/database/database.js
+++ api/src/database/database.js
@@ -10,6 +10,7 @@ const defineModelVideogame = require('../Videogame/models/Videogame.model')
 const defineModelGenre = require('../Genre/models/Genre.model')
 const defineModelParentPlatform = require('../Platform/models/ParentPlatform.model')
 const defineModelChildPlatform = require('../Platform/models/ChildPlatform.model')
+const defineModelTag = require('../Tags/models/Tag.model')
 
 //! Database connection
 const database = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
@@ -22,9 +23,10 @@ defineModelVideogame(database)
 defineModelGenre(database)
 defineModelParentPlatform(database)
 defineModelChildPlatform(database)
+defineModelTag(database)
 
 //! import of models
-const { videogame, genre, childPlatform } = database.models
+const { videogame, genre, childPlatform, tag } = database.models
 
 //! Establishment of relationships
 
@@ -35,6 +37,10 @@ genre.belongsToMany(videogame, { through: 'videogame-genre' })
 //! Many-To-Many
 videogame.belongsToMany(childPlatform, { through: 'videogame-childPlatform' })
 childPlatform.belongsToMany(videogame, { through: 'videogame-childPlatform' })
+
+//! Many-To-Many
+videogame.belongsToMany(tag, { through: 'game-tag' })
+tag.belongsToMany(videogame, { through: 'game-tag' })
 
 module.exports = {
   database,
