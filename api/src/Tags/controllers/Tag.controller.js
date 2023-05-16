9cb0026f457fa6575696fc9a0945c63987e98753
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 20:26:07 -0600

    fix(Tag/controllers): :bug: No se estaba llamando al serviceSaveTagsInDatabase
    Llamamos a nuestro service luego de obtener las tags desde la API de RAWG, estas se las pasamos como parametro a nuestro service que almacena las tags en la base de datos

--- api/src/Tags/controllers/Tag.controller.js
+++ api/src/Tags/controllers/Tag.controller.js
@@ -1,6 +1,7 @@
 //! Services
 const { serviceGetTagsFromAPI } = require('../services/External API RAWG/GetTags.service')
 const { serviceGetTagsFromDatabase } = require('../services/Local Database/GetTags.service')
+const { serviceSaveTagsInDatabase } = require('../services/Local Database/SaveTags.service')
 
 //! Handlers
 const getTags = async (req, res) => {
@@ -11,6 +12,8 @@ const getTags = async (req, res) => {
       res.status(302).send({ status: 'FOUND', data: tagsFromDatabase })
     } else {
       const tags = await serviceGetTagsFromAPI()
+      serviceSaveTagsInDatabase(tags)
+
       res.status(201).send({ status: 'CREATED', data: tags })
     }
   } catch (error) {
