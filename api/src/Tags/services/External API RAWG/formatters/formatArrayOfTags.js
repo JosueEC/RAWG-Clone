f5bd26bb3c9962d74ac7b9c2b026115e611e6335
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 20:06:40 -0600

    feat(Tag/services/API RAWG): :sparkles: feature getTagsFromAPI
    EL endpoint /tags ya dispone de una peticion GET para obtener la tags en el formato esperado en nuestro frontend

--- api/src/Tags/services/External API RAWG/formatters/formatArrayOfTags.js
+++ api/src/Tags/services/External API RAWG/formatters/formatArrayOfTags.js
@@ -1,5 +1,14 @@
 function formatArrayOfTags (arrayOgTags) {
-  return 'formated array of tags'
+  const tags = arrayOgTags.results.map((tag) => {
+    return {
+      id: tag.id,
+      name: tag.name,
+      slug: tag.slug,
+      games_count: tag.games_count,
+      image_background: tag.image_background
+    }
+  })
+  return tags
 }
 
 module.exports = {
