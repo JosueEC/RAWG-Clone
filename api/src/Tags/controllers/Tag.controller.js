148c9b6ce068cce8ce18ff892b29e4e5a8dce7bf
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-15 19:50:51 -0600

    refactor(Tag/services/API RAWG): :recycle: Creacion del serviceGetTagsFromAPI
    Este service nos devuelve la informacion de las tags obtenida desde la API de RAWG
    
    * Hasta aqui solo se creo el metodo, aun falta desarrollar la logica del service

--- api/src/Tags/controllers/Tag.controller.js
+++ api/src/Tags/controllers/Tag.controller.js
@@ -1,6 +1,11 @@
+//! Services
+const { serviceGetTagsFromAPI } = require('../services/External API RAWG/GetTags.service')
+
+//! Handlers
 const getTags = async (req, res) => {
   try {
-    res.status(200).send('controller /tags')
+    const tags = await serviceGetTagsFromAPI()
+    res.status(302).send({ status: 'FOUND', data: tags })
   } catch (error) {
     res.status(404).send({ status: 'FAILED', error: error.message })
   }
---
+++ api/src/Tags/services/External API RAWG/GetTags.service.js
@@ -0,0 +1,7 @@
+const serviceGetTagsFromAPI = async () => {
+  return 'service /tags'
+}
+
+module.exports = {
+  serviceGetTagsFromAPI
+}
