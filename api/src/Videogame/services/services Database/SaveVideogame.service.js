88cfeb34d0b7998dab7512c8aea0d42b52a18efc
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-17 09:29:13 -0600

    fix(Videogame/services/Database): :bug: Se agrego la propiedad slug al controller y service de POST videogame
    Nos faltaba recibir esta propiedad en el body que llega a la peticion y a traves del service pasar esta propiedad al modelo para que sea almacenada
    
    * Hasta aqui aun falta añadir esa propiedad al metodo GET videogame por ID y al GET videogames

--- api/src/Videogame/services/services Database/SaveVideogame.service.js
+++ api/src/Videogame/services/services Database/SaveVideogame.service.js
@@ -3,9 +3,10 @@ const { videogame } = require('../../../database/database')
 
 //! Service
 /* eslint-disable camelcase */
-const serviceSaveVideogameInDatabase = async ({ name, released, background_image, website, rating, rating_top, movies_count, ratings_count, platforms, genres, tags, description_raw }) => {
+const serviceSaveVideogameInDatabase = async ({ name, slug, released, background_image, website, rating, rating_top, movies_count, ratings_count, platforms, genres, tags, description_raw }) => {
   const newVideogame = await videogame.create({
     name,
+    slug,
     released,
     background_image,
     website,
