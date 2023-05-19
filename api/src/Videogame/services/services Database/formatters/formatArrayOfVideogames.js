29886dbab073a306b06ea81bfa59256bcc02e7d3
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-19 11:24:15 -0600

    feat(Videogame/services/Database): :sparkles: Definicion de la logica del formatArrayOfVideogames
    La funcion ya mapea los resultados del service GetVideogames, devolviendo solo los datos que requerimos en el frontend y en el formato establecido para mantener uniformidad con los que sean obtenidos de la API de RAWG

--- api/src/Videogame/services/services Database/formatters/formatArrayOfVideogames.js
+++ api/src/Videogame/services/services Database/formatters/formatArrayOfVideogames.js
@@ -1,5 +1,43 @@
-function formatArrayOfVideogames () {
+function formatArrayOfVideogames (arrayOfVideogames) {
+  const videogames = arrayOfVideogames.map((videogame) => {
+    return {
+      id: videogame.id,
+      slug: videogame.slug,
+      name: videogame.name,
+      released: videogame.released,
+      background_image: videogame.background_image,
+      rating: videogame.rating,
+      rating_top: videogame.rating_top,
+      platforms: formatArrayOfPlatforms(videogame.childPlatforms),
+      genres: formatArrayOfGenres(videogame.genres),
+      tags: formatArrayOfTags(videogame.tags)
+    }
+  })
+  return videogames
+}
 
+function formatArrayOfPlatforms (arrayOfPlatforms) {
+  const platforms = []
+  arrayOfPlatforms.forEach((platform) => {
+    platforms.push(platform.slug)
+  })
+  return platforms
+}
+
+function formatArrayOfGenres (arrayOfGenres) {
+  const genres = []
+  arrayOfGenres.forEach((genre) => {
+    genres.push(genre.slug)
+  })
+  return genres
+}
+
+function formatArrayOfTags (arrayOfTags) {
+  const tags = []
+  arrayOfTags.forEach((tag) => {
+    tags.push(tag.slug)
+  })
+  return tags
 }
 
 module.exports = {
