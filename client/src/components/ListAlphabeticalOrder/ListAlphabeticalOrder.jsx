a28dc5711756dec80a787bf77a3332b48c2768f6
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-05-22 20:12:45 -0600

    feat(client/components): :sparkles: Agregamos la etiqueta Link al elemento Home y Landing en el componente SideBar
    A traves de estos elementos podemos direccionarnos hacia la ruta Landing o hacia la ruta Home, dependiendo de cual seleccionemos
    
    * Hasta aqui aun falta mejora los estilos CSS

--- client/src/components/ListAlphabeticalOrder/ListAlphabeticalOrder.jsx
+++ client/src/components/ListAlphabeticalOrder/ListAlphabeticalOrder.jsx
@@ -15,7 +15,7 @@ export default function ListAlphabeticalOrder () {
 
   return (
     <div className={styles.listAlphabetical}>
-      <select name='selectAlphabetical' onChange={handleChange} className={styles.selectAlphabetical}>
+      <select name='selectAlphabeth' onChange={handleChange} className={styles.selectAlphabetical}>
         <option value='random' selected>Random</option>
         <option value='a-z'>A - Z</option>
         <option value='z-a'>Z - A</option>
--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { ROUTER } from '../../../consts'
 
 import './SideBar.css'
 
 export default function SideBar ({ sidebar }) {
   return (
     <div className={sidebar ? 'sidebar sidebar--open' : 'sidebar'}>
-      <li><i className='fa-solid fa-house-user' />Home</li>
-      <li>Products</li>
-      <li>Carrer</li>
-      <li>Contact Us</li>
-      <li>About Us</li>
+      <Link to={ROUTER.LANDING}><li><i className='fa-solid fa-house-user' />Landing</li></Link>
+      <Link to={ROUTER.HOME}><li>Home</li></Link>
+      <label>Platforms</label>
+      <li>PC</li>
+      <li>PlayStation 4</li>
+      <li>Xbox One</li>
+      <li>Nintendo Switch</li>
+      <li>IOS</li>
+      <li>Android</li>
+      <label>Genres</label>
+      <li>Action</li>
+      <li>Strategy</li>
+      <li>RPG</li>
+      <li>Shooter</li>
+      <li>Adventure</li>
+      <li>Puzzle</li>
+      <li>Racing</li>
+      <li>Sports</li>
     </div>
   )
 }
