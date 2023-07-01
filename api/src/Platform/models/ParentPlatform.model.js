e124b9b6213b89078fd6aef95c6fc0c13db9df3c
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-30 21:44:21 -0600

    feat(controllers): 🦺 Addition of the fields validations to the ParentPlatform model.

--- api/src/Platform/models/ParentPlatform.model.js
+++ api/src/Platform/models/ParentPlatform.model.js
@@ -7,9 +7,15 @@ module.exports = (database) => {
     id: {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV1,
+      allowNull: false,
       primaryKey: true,
       validate: {
-        isUUid: true
+        notEmpty: {
+          msg: 'parentPlaform ID cannot be empty'
+        },
+        isUUid: {
+          msg: 'parentPlatform ID must be a valid UUID'
+        }
       }
     },
     name: {
@@ -17,7 +23,16 @@ module.exports = (database) => {
       allowNull: false,
       unique: true,
       validate: {
-        len: [2, 50]
+        notEmpty: {
+          msg: 'parentPlatform name cannot be empty'
+        },
+        isLowercase: {
+          msg: 'parentPaltform name must be lowercase'
+        },
+        len: {
+          args: [2, 50],
+          msg: 'parentPlatform name must be between 2 and 50 characters in length'
+        }
       }
     },
     slug: {
@@ -25,10 +40,20 @@ module.exports = (database) => {
       allowNull: false,
       unique: true,
       validate: {
-        len: [2, 50]
+        notEmpty: {
+          msg: 'parentPlatform slug cannot be empty'
+        },
+        isLowercase: {
+          msg: 'parentPlatform slug must be lowercase'
+        },
+        len: {
+          args: [2, 50],
+          msg: 'parentPlatform slug must be between 2 and 50 characters in length'
+        }
       }
     }
   }, {
-    timestamps: false
+    timestamps: false,
+    freezeTableName: true
   })
 }
