753566b190e5d19845059dc997c39245c59462e1
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-06-30 21:23:02 -0600

    refactor(controllers): ♻️ Change of the response functions in the Tag controller using httpResponse module.

--- api/src/Tags/controllers/Tag.controller.js
+++ api/src/Tags/controllers/Tag.controller.js
@@ -3,21 +3,25 @@ const { serviceGetTagsFromAPI } = require('../services/services API RAGW/GetTags
 const { serviceGetTagsFromDatabase } = require('../services/services Database/GetTags.service')
 const { serviceSaveTagsInDatabase } = require('../services/services Database/SaveTags.service')
 
+//! Utils
+const { httpResponse } = require('../../utils/httpResponse')
+const { HTTP_FOUND, HTTP_CREATED, HTTP_NOT_FOUND } = require('../../utils/httpCodes')
+
 //! Handlers
 const getTags = async (req, res) => {
   try {
     const tagsFromDatabase = await serviceGetTagsFromDatabase()
 
     if (tagsFromDatabase.length !== 0) {
-      res.status(302).send({ status: 'FOUND', data: tagsFromDatabase })
+      httpResponse(res, HTTP_FOUND, tagsFromDatabase)
     } else {
       const tags = await serviceGetTagsFromAPI()
       serviceSaveTagsInDatabase(tags)
 
-      res.status(201).send({ status: 'CREATED', data: tags })
+      httpResponse(res, HTTP_CREATED, tags)
     }
   } catch (error) {
-    res.status(404).send({ status: 'FAILED', error: error.message })
+    httpResponse(res, HTTP_NOT_FOUND, error.message)
   }
 }
 
