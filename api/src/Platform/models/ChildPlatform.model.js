1095cf420326413985683dde2681395c9cdbbefd
J Evangelista Cruz <josueev.cruz@gmail.com>
2023-07-01 18:37:15 -0600

    feat(models): 🦺 Addition of the field validations to the ChildPlatform model.

--- api/src/Platform/models/ChildPlatform.model.js
+++ api/src/Platform/models/ChildPlatform.model.js
@@ -7,9 +7,17 @@ module.exports = (database) => {
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
+      allowNull: false,
       validate: {
-        isNumeric: true,
-        isInt: true
+        notEmpty: {
+          msg: 'child platform ID cannot be empty'
+        },
+        isNumeric: {
+          msg: 'child platform ID must be a numerical value'
+        },
+        isInt: {
+          msg: 'child platform ID must be a valid integer'
+        }
       }
     },
     name: {
@@ -17,7 +25,16 @@ module.exports = (database) => {
       allowNull: false,
       unique: true,
       validate: {
-        len: [2, 50]
+        notEmpty: {
+          msg: 'child platform name cannot be empty'
+        },
+        isLowercase: {
+          msg: 'child platform name must be lowercase'
+        },
+        len: {
+          args: [2, 50],
+          msg: 'child platform name must be between 2 and 50 characters'
+        }
       }
     },
     slug: {
@@ -25,7 +42,16 @@ module.exports = (database) => {
       allowNull: false,
       unique: true,
       validate: {
-        len: [2, 50]
+        notEmpty: {
+          msg: 'child platform slug cannot be empty'
+        },
+        isLowercase: {
+          msg: 'child platform slug must be lowercase'
+        },
+        len: {
+          args: [2, 50],
+          msg: 'child platform slug must be between 2 and 50 characters in length'
+        }
       }
     },
     games_count: {
@@ -33,18 +59,31 @@ module.exports = (database) => {
       allowNull: false,
       defaultValue: 0,
       validate: {
-        isNumeric: true,
-        isInt: true
+        notEmpty: {
+          msg: 'child platform games count cannot be empty'
+        },
+        isNumeric: {
+          msg: 'child platform games count must be a numerical value'
+        },
+        isInt: {
+          msg: 'child platform games count muste be a valid integer'
+        }
       }
     },
     image_background: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
-        isUrl: true
+        notEmpty: {
+          msg: 'child platform image cannot be empty'
+        },
+        isUrl: {
+          msg: 'child platform image must be a valida image URL'
+        }
       }
     }
   }, {
-    timestamps: false
+    timestamps: false,
+    freezeTableName: true
   })
 }
